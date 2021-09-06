2e4a404b74cb5b7d7259d60ba8ce89ca0da41a46
xhlulu <xhlperso@gmail.com>
2021-09-06 01:48:24 -0400

    Allow update method to recursively monkey-patch to component

--- static/main.py
+++ static/main.py
@@ -6,9 +6,15 @@ import js.MaterialUI as mui
 import js.React.createElement as e
 
 
-
 def jsobj(**kwargs):
     return js.Object.fromEntries(pyodide.to_js(kwargs))
+
+
+def __to_camel_case(snake_str):
+    components = snake_str.split("_")
+    # We capitalize the first letter of each component except the first one
+    # with the 'title' method and join them together.
+    return components[0] + "".join(x.title() for x in components[1:])
 
 
 def pythonify(component):
@@ -16,10 +22,25 @@ def pythonify(component):
     Makes a react component feel like you are calling a function
     """
 
-    def aux(*args, **kwargs):
-        return js.React.createElement(component, jsobj(**kwargs), *args)
-    
-    return aux
+    def aux(self, old_args, old_kwargs, *args, **kwargs):
+        kwargs = {__to_camel_case(k): v for k, v in kwargs.items()}
+        if old_args is None:
+            old_args = []
+        if old_kwargs is None:
+            old_kwargs = {}
+
+        old_args.extend(args)
+        old_kwargs.update(kwargs)
+
+        args = old_args
+        kwargs = old_kwargs
+
+        rc = js.React.createElement(component, jsobj(**kwargs), *args)
+        rc.update = partial(aux.__get__(rc, rc.__class__), args, kwargs)
+
+        return rc
+
+    return partial(aux, component, None, None)
 
 
 Typography = pythonify(mui.Typography)
@@ -58,29 +79,27 @@ def LightBulbIcon(props, children):
         path(
             d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7zm2.85 11.1l-.85.6V16h-4v-2.3l-.85-.6C7.8 12.16 7 10.63 7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.63-.8 3.16-2.15 4.1z"
         ),
-        **props.to_py()
+        **props.to_py(),
     )
 
 
 @pythonify
 def ProTip(props, children):
     classes = use_styles()
-    return Typography(
-        LightBulbIcon(className=classes.light_bulb),
+    return Typography(class_name=classes.root, color="textSecondary").update(
+        LightBulbIcon(class_name=classes.light_bulb),
         "Pro tip: See more ",
         Link(
             "templates",
             href="https://material-ui.com/getting-started/templates/",
         ),
         " on the Material-UI documentation.",
-        className=classes.root,
-        color="textSecondary",
     )
 
 
 @pythonify
 def Copyright(props, children):
-    return Typography(
+    return Typography(variant="body2", color="textSecondary", align="center").update(
         "Copyright © ",
         Link(
             "Your Website",
@@ -88,27 +107,23 @@ def Copyright(props, children):
             color="inherit",
         ),
         f" {datetime.datetime.now().year}.",
-        variant="body2",
-        color="textSecondary",
-        align="center",
     )
 
 
 @pythonify
 def App(props, children):
-    return Container(
+    return Container(maxWidth="sm").update(
         div(
             Typography(
                 "CDN v4-beta example",
                 variant="h4",
                 component="h1",
-                gutterBottom=True,
+                gutter_bottom=True,
             ),
             ProTip(),
             Copyright(),
             style=dict(marginTop=24),
         ),
-        maxWidth="sm",
     )
 
 
